ca6d855c7aae2bc83fd465639371ad0189e9c70b
Mahmoud Abdulrazek Ali <mahmood.abdelrazek@outlook.com>
2025-01-07 23:26:48 +0200

    Added emitter path to state_streaming_command.dart

--- lib/state_streaming_command.dart
+++ lib/state_streaming_command.dart
@@ -3,5 +3,6 @@ export 'src/implementations/action_state_streaming_command.dart';
 export 'src/implementations/errors.dart';
 export 'src/interfaces/action_state.dart';
 export 'src/interfaces/command.dart';
+export 'src/interfaces/emitter.dart';
 export 'src/interfaces/error.dart';
 export 'src/interfaces/state_streaming_command.dart';
